2f387786bf0c8a5c879f8d10e83be5fa4bc80512
Ofor David <davidoforincloud@gmail.com>
2025-08-11 18:16:22 +0100

    added second subnet for alb

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -18,14 +18,24 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_a" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.name}-public-subnet"
+    Name = "${var.name}-public-a-subnet"
+  }
+}
+resource "aws_subnet" "public_b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-b-subnet"
   }
 }
 
@@ -41,7 +51,12 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_subnet_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_subnet_b" {
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
