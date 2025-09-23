97ad47d97d2d0e9a862fdebbde1722d5ab3cf9a1
Ofor David <davidoforincloud@gmail.com>
2025-09-23 12:01:29 +0100

    ecr stable

--- terraform/modules/ecr/output.tf
+++ terraform/modules/ecr/output.tf
@@ -2,3 +2,8 @@ output "repo_url" {
   description = "The URL of the ECR repository"
   value       = aws_ecr_repository.ecr_repo.repository_url
 }
+
+output "repo_name"{
+  description = "The name of the ECR repository"
+  value       = aws_ecr_repository.ecr_repo.name
+}
